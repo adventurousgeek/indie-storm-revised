728a6539b24fc64e50f6de93c87b4de2457853cc
DESKTOP-GTEC5V2\devzain <geekspod@gmail.com>
2018-07-31 11:52:43 +0500

    Hover Effects Added!

--- common-components/footer.php
+++ common-components/footer.php
@@ -3,7 +3,7 @@
     <section class="pre-footer">
         <div class="container">
             <div class="row">
-                <div class="col-xl-4">
+                <div class="col-xl-4 col-lg-4">
                     <h2>Gamers FAQ</h2>
                     <ul class="list-unstyled list-inline">
                         <li class="list-inline-item"><a href="#">Duis vulputate velit at sem maximus<br />elementum aliquam ac nisl eget?</a></li>
@@ -12,8 +12,8 @@
                     </ul>
                     <a href="#">View All</a>
                 </div>
-                <div class="col-xl-4">
-                    <h2>Gamers FAQ</h2>
+                <div class="col-xl-4 col-lg-4">
+                    <h2>DEVELOPERS FAQ</h2>
                     <ul class="list-unstyled list-inline">
                         <li class="list-inline-item"><a href="#">Aecenas luctus ex eu ligula egestas<br />consequat. Morbi sit amet nulla ut dolor?</a></li>
                         <li class="list-inline-item"><a href="#">Quisque pellentesque quam id dui<br />convallis, non laoreet nunc finibus?</a></li>
@@ -21,8 +21,8 @@
                     </ul>
                     <a href="#">View All</a>
                 </div>
-                <div class="col-xl-4">
-                    <h2>Gamers FAQ</h2>
+                <div class="col-xl-4 col-lg-4">
+                    <h2>STREAMERS & VIEWERS FAQ</h2>
                     <ul class="list-unstyled list-inline">
                         <li class="list-inline-item"><a href="#">Proin pretium hendrerit nisl non ultrices.<br />Praesent iaculis vehicula ipsum nec?</a></li>
                         <li class="list-inline-item"><a href="#">Curabitur urna turpis, interdum vel lobortis<br />sed, venenatis sit amet mi?</a></li>
@@ -36,7 +36,7 @@
     <section class="post-footer">
         <div class="container">
             <div class="row">
-                <div class="col-xl-2">
+                <div class="col-xl-2 col-lg-2">
                     <h2>Site Map</h2>
                     <ul class="list-unstyled">
                         <li><a href="#">Market</a></li>
@@ -46,7 +46,7 @@
                         <li><a href="#">ICO Info</a></li>
                     </ul>
                 </div>
-                <div class="col-xl-3">
+                <div class="col-xl-3 col-lg-3">
                     <h2>Follow Us</h2>
                     <ul class="list-inline list-unstlyed">
                         <li class="list-inline-item btn-glitch"><a href="#"><svg class="footer-mail-icon"><use xlink:href="#footer-mail-icon"></use></svg></a></li>
@@ -57,7 +57,7 @@
                         <li class="list-inline-item btn-glitch"><a href="#"><svg class="bitcoin-wallet-footer"><use xlink:href="#bitcoin-wallet-footer"></use></svg></a></li>
                     </ul>
                 </div>
-                <div class="col-xl-3">
+                <div class="col-xl-3 col-lg-3">
                     <h2>Documents</h2>
                     <ul class="list-unstyled">
                         <li class="btn-glitch"><a href="#"><svg class="document-download-icon"><use xlink:href="#document-download-icon"></use></svg>Whitepaper</a></li>
@@ -65,17 +65,20 @@
                         <li class="btn-glitch"><a href="#"><svg class="document-download-icon"><use xlink:href="#document-download-icon"></use></svg>Market Research</a></li>
                     </ul>
                 </div>
-                <div class="col-xl-4 text-right">
-                    <h2>Subscribe For Updates</h2>
-                    <form>
-                        <svg class="footer-subscription-border"><use xlink:href="#footer-subscription-border"></use></svg>
-                        <button type="submit" class="btn btn-primary btn-glitch">Notify Me</button>
-                        <div class="form-group">
-                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="email address">
-                        </div>
-                    </form>
+                <div class="col-xl-4 col-lg-4 text-right">
+                    <div class="footer-last-wrapper">
+                        <h2>Subscribe For Updates</h2>
+                        <form>
+                            <svg class="footer-subscription-border"><use xlink:href="#footer-subscription-border"></use></svg>
+                            <button type="submit" class="btn btn-primary btn-glitch">Notify Me</button>
+                            <div class="form-group">
+                                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="email address">
+                            </div>
+                        </form>
 
-                    <h2 class="connect-slack-join">Join Our Slack</h2><button type="submit" class="btn btn-primary join-slack btn-glitch"><svg class="connect-slack-border"><use xlink:href="#connect-slack-border"></use></svg>Connect</button>
+                        <h2 class="connect-slack-join">Join Our Slack</h2><button type="submit" class="btn btn-primary join-slack btn-glitch"><svg class="connect-slack-border"><use xlink:href="#connect-slack-border"></use></svg>Connect</button>
+                        <svg class="footer-background-logo"><use xlink:href="#footer-background-logo"></use></svg>
+                    </div>
 
                 </div>
             </div>
